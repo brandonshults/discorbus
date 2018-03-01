6728fe40cec45e82f1654af2e47a28547bad2be9
Brandon Shults <brandon.shults@gmail.com>
2018-02-28 16:56:56 -0800

    Improve roadmap in preparation for actual releases.

--- commands/roadmap.js
+++ commands/roadmap.js
@@ -2,40 +2,52 @@ const { commandKey } = require('../bot/config');
 
 const moment = require('moment-timezone');
 
-const dates =
+const roadmap =
   new Map([
-    [moment.tz('2018-03-05 12:00', 'America/Chicago'), "warrior hit detection, improved tutorial, kill quests"],
-    [moment.tz('2018-03-19 12:00', 'America/Chicago'), "new talent system, journal tabs, fishing gear"],
-    [moment.tz('2018-03-26 12:00', 'America/Chicago'), "shard dungeons"],
-    [moment.tz('2018-04-09 12:00', 'America/Chicago'), 'pet overhaul'],
-    [moment.tz('2018-04-23 12:00', 'America/Chicago'), "new world boss, tournament of mages, fishing updates"],
-    [moment.tz('2018-05-07 12:00', 'America/Chicago'), "act 3, new PvP mechanics"],
-    [moment.tz('2018-05-28 12:00', 'America/Chicago'), "raid dungeons"]
+    [moment.tz('2018-03-05 12:00', 'America/Chicago'), "Dye! Warrior hit detection, improved tutorial, kill quests"],
+    [moment.tz('2018-03-19 12:00', 'America/Chicago'), "New talent system, journal tabs, fishing gear"],
+    [moment.tz('2018-03-26 12:00', 'America/Chicago'), "Shard dungeons"],
+    [moment.tz('2018-04-09 12:00', 'America/Chicago'), 'Pet overhaul'],
+    [moment.tz('2018-04-23 12:00', 'America/Chicago'), "New world boss, tournament of mages, fishing updates"],
+    [moment.tz('2018-05-07 12:00', 'America/Chicago'), "Act 3, new PvP mechanics"],
+    [moment.tz('2018-05-28 12:00', 'America/Chicago'), "Raid dungeons"],
   ]);
 
-function getFormattedRoadmap() {
-  const currentDate = moment.tz('America/Chicago');
-  const futureDates = Array.from(dates.keys())
-    .filter(dateToCheck => dateToCheck.diff(currentDate) >= 0)
-    .sort((left, right) => left.isBefore(right) ? -1 : 1);
-
-  const formattedEntries = futureDates.map(futureDate => {
-    const days = Math.floor(moment.duration(futureDate.diff(currentDate), 'milliseconds').asDays());
+function formatDatesForOutput(dates, currentDate) {
+  const formattedEntries = dates.map(date => {
+    const days = Math.floor(moment.duration(date.diff(currentDate), 'milliseconds').asDays());
 
     return {
-      date: futureDate.format('MMM Do'),
+      date: date.format('MMM Do'),
       duration: `(${days}d):`,
-      releaseNotes: dates.get(futureDate)
+      releaseNotes: roadmap.get(date)
     }
   });
 
-  const longestDate = formattedEntries.reduce((longest, { date }) => date.length > longest ? date.length : longest,0);
-  const longestDuration = formattedEntries.reduce((longest, { duration }) => duration.length > longest ? duration.length : longest,0);
+  const longestDate = formattedEntries.reduce((longest, { date }) => date.length > longest ? date.length : longest, 0);
+  const longestDuration = formattedEntries.reduce((longest, { duration }) => duration.length > longest ? duration.length : longest, 0);
+
+  return formattedEntries.reduce((output, { date, duration, releaseNotes}, index) => {
+    return output + `${index > 0 ? '\n' : ''}[${date.padEnd(longestDate, ' ')}]${duration.padEnd(longestDuration, ' ')} ${releaseNotes}`;
+  }, '')
+
+}
+
+function getFormattedRoadmap() {
+  const currentDate = moment.tz('America/Chicago');
+  const futureDates = Array.from(roadmap.keys())
+    .filter(dateToCheck => dateToCheck.diff(currentDate) >= 0)
+    .sort((left, right) => left.isBefore(right) ? -1 : 1);
+
+  const pastDates = Array.from(roadmap.keys())
+    .filter(dateToCheck => dateToCheck.diff(currentDate) < 0)
+    .sort((left, right) => left.isAfter(right) ? -1 : 1)
+    .slice(0, 3);
 
   return `\`\`\`md
-${formattedEntries.reduce((output, { date, duration, releaseNotes}, index) => {
-    return output + `${index > 0 ? '\n' : ''}[${date.padEnd(longestDate, ' ')}]${duration.padEnd(longestDuration, ' ')} ${releaseNotes}`;
-  }, '')}\`\`\``
+${futureDates.length > 0 ? 'Upcoming:\n' + formatDatesForOutput(futureDates, currentDate) : ''}
+
+${pastDates.length > 0 ? 'Past:\n' + formatDatesForOutput(pastDates, currentDate) : ''}\`\`\``
 }
 
 module.exports = {
---
+++ commands/time.js
@@ -0,0 +1,104 @@
+const moment = require("moment-timezone");
+
+// See http://momentjs.com/timezone/ for more zones.
+// const trackedZones = {
+//   EST: "America/Toronto", // EST
+//   CST: "America/Resolute", // CST
+//   MST: "America/Edmonton", // MST
+//   PST: "America/Vancouver", // PST
+// };
+const trackedZones = {
+  "EST": "America/Toronto",
+  "CST": "America/Resolute",
+  "MST": "America/Edmonton",
+  "PST": "America/Vancouver"
+};
+
+const getZoneByAbbrev = function(searchFor) {
+  searchFor = searchFor.toUpperCase();
+  return Object.keys(trackedZones).includes(searchFor) ? trackedZones[searchFor] : null;
+};
+
+const helpMessage = "Please enter requests in the format **!time 11:11 xm ZZZ** (*Ex: 12:35 pm EST*), or just enter **!time** by itself (with no parameters) for the current server time.";
+const genericErrorMessage = "Invalid time request. " + helpMessage;
+const availableZones = Object.keys(trackedZones).join("/");
+
+module.exports = {
+  usage: "",
+  name: "time",
+  fn: (args, msg) => {
+    // console.log(timeCmd);
+    let inMoment = null;
+
+    const currentTime = moment();
+
+    if(args.length === 0) {
+      inMoment = currentTime;
+    } else if(args[0] === 'help') {
+      msg.channel.send(helpMessage + "\nAvailable Zones: ["+ availableZones +"]");
+      return;
+    } else if(args.length < 2) { // need at least 2 arguments. Ex: 9am, EST
+      msg.channel.send(genericErrorMessage);
+      return;
+    } else {
+
+      args = args.map(a => a.toUpperCase());
+
+      const inZone =  ["AM", "PM"].includes(args[1]) ? args[2].toUpperCase() : args[1];
+      if(!(inZone in trackedZones)) {
+        msg.channel.send("I'm sorry. I'm not tracking the timezone: " + inZone);
+      }
+
+      const inTime = args[0] + ( ["AM", "PM"].includes(args[1]) ? " " + args[1] : "" );
+
+      inMoment = moment(inTime, "h:mm a").isValid() ? moment(inTime, "h:mm a")
+        : moment(inTime, "h:mma").isValid() ? moment(inTime, "h:mma")
+          : moment(inTime, "h a").isValid() ? moment(inTime, "h a")
+            : moment(inTime, "ha").isValid() ? moment(inTime, "ha") : null;
+
+      if(!moment.isMoment(inMoment)) {
+        msg.channel.send(genericErrorMessage);
+        return;
+      }
+
+      let zoneName = getZoneByAbbrev(inZone)
+      if(zoneName !== null) {
+        // Timezone is changed, but moment in time remains the same (really only offsets the time reading),
+        // so we'll set the hour and minute explicitly below.
+        let zoneMoment = inMoment.clone().tz(zoneName);
+
+        // Had to add this hack for h:mma format which was incorrectly reading pm times when no space was used after the numeric portion of the time.
+        if(inMoment.get("hour") < 12 && inTime.toLowerCase().indexOf("pm") !== -1) {
+          zoneMoment.set("hour", inMoment.get("hour") + 12);
+        } else {
+          zoneMoment.set("hour", inMoment.get("hour"));
+        }
+        zoneMoment.set("minute", inMoment.get("minute"));
+
+        inMoment = zoneMoment;
+      } else {
+        msg.channel.send("Sorry. That's not a valid timezone.");
+        return;
+      }
+
+    }
+
+    const irlTime = inMoment || moment(); // use inMoment if it was provided as a param, otherwise use the current time.
+
+    let outMsg = "```fix\nTime by Zone: ";
+    for(let prop in trackedZones) {
+      const thisTime = irlTime.clone().tz(trackedZones[prop])
+      outMsg += "\n" + thisTime.format(" [[]z[]] h:mm a");
+    }
+
+    if(inMoment !== currentTime) {
+      const hours = moment.duration(irlTime.diff(currentTime)).asHours();
+      outMsg += "\n(" + hours.toFixed(2) + " hours)";
+    }
+
+    outMsg += "\n```";
+
+    msg.channel.send(outMsg);
+
+  },
+};
