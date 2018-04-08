cb65bb85d939670e95510d579a565cb2a0cd47e9
Brandon Shults <brandon.shults@gmail.com>
2018-04-07 20:16:17 -0700

    Allow friends of Nox to use the bot.

--- bot/config.js
+++ bot/config.js
@@ -2,8 +2,9 @@ module.exports = Object.freeze({
   commandKey: '!',
   whiteList: {
     roles: [
-      { name: 'Noxian Extraordinaire', id: '402162558200578048' },    // Nox Main Server
-      { name: 'Tester', id: '418270718510366749' }                    // Discorbus Testing Server
+      { name: 'Noxian Extraordinaire', id: '402162558200578048' },     // Nox Main Server
+      { name: 'Friends of Nox', id: '402163922649481226' },            // Nox Main Server
+      { name: 'Tester', id: '418270718510366749' }                     // Discorbus Testing Server
     ]
   },
   guildies: [
