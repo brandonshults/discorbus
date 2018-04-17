ebbadb5c1fc89fbe6aa493684ce038c97507156a
Brandon Shults <brandon.shults@gmail.com>
2018-04-17 09:06:07 -0700

    Add new roadmap items.

--- commands/roadmap.js
+++ commands/roadmap.js
@@ -8,7 +8,9 @@ const roadmap =
     [moment.tz('2018-03-19 12:00', 'America/Chicago'), 'New talent system, VoIP rework, journal tabs, fishing gear'],
     [moment.tz('2018-03-30 12:00', 'America/Chicago'), 'Shard dungeons'],
     [moment.tz('2018-04-16 12:00', 'America/Chicago'), 'Gear re-rolling, shard gear remodel, beginning of new sound design, portal system overhaul, pet overhaul'],
-    [moment.tz('2018-04-23 12:00', 'America/Chicago'), 'New world boss, tournament of mages, fishing updates'],
+    [moment.tz('2018-04-23 12:00', 'America/Chicago'), 'Fishing Quests, rare fish'],
+    [moment.tz('2018-04-27 12:00', 'America/Chicago'), 'New world boss'],
+    [moment.tz('2018-04-28 12:00', 'America/Chicago'), 'Tournament of Mages'],
     [moment.tz('2018-05-07 12:00', 'America/Chicago'), 'Act 3, new PvP mechanics'],
     [moment.tz('2018-05-28 12:00', 'America/Chicago'), 'Raid dungeons']
   ]);
