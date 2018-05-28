833504a4b4094bb7a5893af99816b112ba300d4c
Brandon Shults <brandon.shults@gmail.com>
2018-05-28 05:55:40 -0700

    Whitelist ARCers

--- bot/config.js
+++ bot/config.js
@@ -4,7 +4,8 @@ module.exports = Object.freeze({
     roles: [
       { name: 'Noxian Extraordinaire', id: '402162558200578048' },     // Nox Main Server
       { name: 'Friends of Nox', id: '402163922649481226' },            // Nox Main Server
-      { name: 'Tester', id: '418270718510366749' }                     // Discorbus Testing Server
+      { name: 'Tester', id: '418270718510366749' },                    // Discorbus Testing Server
+      { name: 'ARCer', id: '415691466451451914' }                      // ARC
     ]
   },
   guildies: [
